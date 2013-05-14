93eb70249c0da7c010be27affbbf3a5f769e7d50
Jason Mitcheson <jmitcheson@gmail.com>
2013-05-14 21:32:35 +1000

    copy/paste existing quickfire chrome extension

---
+++ quickfire-chrome.js
@@ -0,0 +1,35 @@
+
+
+
+
+var QuickfireChrome = function() {
+	
+	var self = this;
+	
+	var port = chrome.extension.connect({name:"quickfire-chrome"});
+	
+	this.tellPageThatChromeExtensionIsPresent = function() {
+		localStorage.setItem("qf.chrome", "true");
+	}
+	
+	this.addHandlers = function() {
+		
+		window.addEventListener("message", function(event) {
+			
+			console.log("Content script: message received", event);
+		    if (event.data.rx && (event.data.rx == "devtools" || event.data.rx == "background")) {
+		      console.log("Content script received (from page)", event.data);
+		      port.postMessage(event.data);
+		    }
+		}, false);
+
+		port.onMessage.addListener(function(request, sender, sendResponse) {
+		    console.log("Content script: message received (from background)", request);
+		    window.postMessage(request, "*");
+		});
+	}
+}
+
+var q = new QuickfireChrome();
+q.addHandlers();
+q.tellPageThatChromeExtensionIsPresent();
